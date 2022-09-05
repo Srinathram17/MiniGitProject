c8701ae7fb2ccddbe77d2bbd63825c318bfdda02
srinathram17 <srinathram17@gmail.com>
2022-09-05 23:42:59 +0530

    Mini Git Project

---
+++ src/test/java/org/jjjjjunittt/test/JUnittttttttttttttt.java
@@ -0,0 +1,69 @@
+package org.jjjjjunittt.test;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JUnittttttttttttttt  extends BaseClass {
+
+	
+	@BeforeClass
+	public static void BeforeClass() {
+		browserlaunch();
+		time(50);
+	}
+	
+	@Before
+	public void Before() {
+		Date d = new Date();
+		System.out.println(d);
+	}
+	@Before
+	public void Before1() {
+		Date d = new Date();
+		System.out.println(d);
+	}
+	
+	
+	@Test
+	public void Test() {
+		urlLaunch("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("9840482080");
+		driver.findElement(By.id("pass")).sendKeys("9840482080");
+		driver.findElement(By.name("login")).click();
+		
+	}
+	
+	@Ignore
+	@Test
+	public void Test1() {
+		urlLaunch("https://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("9840482080");
+		driver.findElement(By.id("pass")).sendKeys("9840482080");
+		driver.findElement(By.name("login")).click();
+		}
+	
+	@After
+	public void After() {
+		Date d = new Date();
+		System.out.println(d);
+	}
+	
+	@AfterClass
+	public static void AfterClass() {
+		quitt(driver);
+	}
+	
+	
+	
+}
---
+++ src/test/resources/org/base/maven/BaseClass.java
@@ -0,0 +1,112 @@
+package org.base.maven;
+
+import java.time.Duration;
+
+import javax.swing.SortOrder;
+import javax.swing.plaf.BorderUIResource.EmptyBorderUIResource;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.browser.Browser;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+
+//	Declared webdriver in instance variable so common for all parent and child
+	public static WebDriver driver;
+
+//	Launch the browser from parent class without creating object 	
+	public static WebDriver browserlaunch() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		return driver;
+	}
+
+//	URL Launch
+
+	public static void urlLaunch(String url) {
+		driver.get(url);
+	}
+
+//	Implicitlywait and maximize the screen method 
+//	and apply parameter for calling seconds manually
+
+	public static void time(int sec) {
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(sec));
+		driver.manage().window().maximize();
+	}
+
+//	Clear
+
+	public static void clear(WebElement c) {
+		c.clear();
+
+	}
+
+//	Sendkeys 
+	public static void sendKeys(WebElement s, String value) {
+		s.sendKeys(value);
+	}
+
+//	Click
+	public static void click(WebElement btn) {
+		btn.click();
+
+	}
+//	Dropdowns step below
+
+	public static void dropDown(WebElement element, String option, String value) {
+		Select s = new Select(element);
+
+		if (option.equalsIgnoreCase("ByIndex")) {
+			int index = Integer.parseInt(value);
+			s.selectByIndex(index);
+		} else if (option.equalsIgnoreCase("ByValue")) {
+			int index = Integer.parseInt(value);
+			s.selectByIndex(index);
+		} else if (option.equalsIgnoreCase("ByVisibletext")) {
+			s.selectByVisibleText(value);
+		}
+	}
+//		Print Order number 
+//		
+//		public static String orderNumber(WebElement or) {
+//			attribute = or.getAttribute("value");
+//			  return attribute;
+
+//		}
+
+//		close the browser
+
+	public static void quitt(WebDriver q) {
+		q.quit();
+
+	}
+
+//		Alert - Always use Switchto.alert();
+
+	public static void simpleAlert() {
+		Alert aa = driver.switchTo().alert();
+		aa.accept();
+	}
+
+	public static void confirmAl() {
+		Alert ac = driver.switchTo().alert();
+		ac.accept();
+		ac.dismiss();
+	}
+	public static void promptAl() {
+		Alert ap = driver.switchTo().alert();
+		ap.accept();
+		ap.dismiss();
+		ap.sendKeys("Done");
+	}
+	
+
+	
+	}
+
